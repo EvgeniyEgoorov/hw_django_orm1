9abc0d01cdaa3a29a6641a1fb0a7dcf49f2d3dac
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-04-09 16:24:29 +0400

    views functions created

--- phones/models.py
+++ phones/models.py
@@ -6,5 +6,5 @@ class Phone(models.Model):
     image = models.URLField(default=None)
     price = models.IntegerField(default=None)
     release_date = models.DateField(default=None)
-    lte_exists = models.BooleanField( default=None)
+    lte_exists = models.BooleanField(default=None)
     slug = models.SlugField(max_length=150, default=None)
--- phones/views.py
+++ phones/views.py
@@ -8,11 +8,26 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    raw_list = Phone.objects.all()
+    sorting = request.GET.get('sort')
+    if sorting:
+        if sorting == 'name':
+            sorted_list = raw_list.order_by('name')
+        elif sorting == 'min_price':
+            sorted_list = raw_list.order_by('price')
+        elif sorting == 'max_price':
+            sorted_list = raw_list.order_by('-price')
+        elif sorting == 'release_date':
+            sorted_list = raw_list.order_by('release_date')
+    else:
+        sorted_list = raw_list
+    context = {'phones': sorted_list}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
     return render(request, template, context)
+
